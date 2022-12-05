ae845dcc94f261b5443ad7f8697fe8c7159955bb
jho378 <jho378@naver.com>
2022-12-05 14:01:40 +0900

    deploy 1st

--- client/src/components/Card/AssetDetailCard/index.tsx
+++ client/src/components/Card/AssetDetailCard/index.tsx
@@ -7,23 +7,26 @@ const AssetDetailCard = () => {
     return (
         <div className="asset-detail-card-wrapper">
             <div id="asset-detail-card-upper">
-                <img className="asset-detail-card-image" src="" alt="" />
+                <img className="asset-detail-card-image" src={`https://images.therich.io/images/logo/us/AAPL.png`} alt="" />
                 <div className="asset-detail-ticker-info">
-                    <div className="asset-detail-card-ticker">AAPL</div>
+                    <div className="asset-detail-card-ticker asset-detail-card-ticker-text">AAPL</div>
                     <div className="asset-detail-card-name">apple</div>                    
                 </div>
-                <div className="asset-detail-quantity">10개</div>
+                <div className="asset-detail-quantity">
+                    <span className="asset-detail-quantity-text">수량</span>
+                    <span className="asset-detail-quantity-count">10</span>
+                </div>
             </div>
             <div id="asset-detail-card-financial">
                 <div className="asset-detail-card-financial-upper">
-                    <div className="asset-detail-sum"></div>
-                    <div className="asset-detail-return"></div>
-                    <div className="asset-detail-dividend"></div>
+                    <div className="asset-detail-sum asset-detail-card-text">자산가치</div>
+                    <div className="asset-detail-return asset-detail-card-text">수익</div>
+                    <div className="asset-detail-dividend asset-detail-card-text">배당월</div>
                 </div>
                 <div className="asset-detail-card-financial-bottom">
-                    <div className="asset-detail-buy-price"></div>
-                    <div className="asset-detail-current-price"></div>
-                    <div className="asset-dividend-rate"></div>
+                    <div className="asset-detail-buy-price asset-detail-card-text">구매가</div>
+                    <div className="asset-detail-current-price asset-detail-card-text">현재가</div>
+                    <div className="asset-dividend-rate asset-detail-card-text">투자배당률</div>
                 </div>
             </div>
         </div>
