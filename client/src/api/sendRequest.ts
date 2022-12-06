3fa2dd2cfa69ab8ca312a1d6618e3965941ee1b4
jho378 <jho378@naver.com>
2022-12-07 00:04:33 +0900

    recovered?

--- client/src/api/sendRequest.ts
+++ client/src/api/sendRequest.ts
@@ -5,6 +5,7 @@ const baseUrl = process.env.NODE_ENV === 'production'
     ? process.env.REACT_APP_PRODUCTION_URL
     : process.env.REACT_APP_DEVELOPMENT_ENV;
 
+
 const baseHeaders = {};
 
 type RequestParamsType = {
