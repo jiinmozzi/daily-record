7ba218659770b909fd06b38f2983cf4ca8c6839c
jho378 <jho378@naver.com>
2022-12-07 15:26:53 +0900

    cors error

--- server/app.ts
+++ server/app.ts
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use(cors({
-    origin: ["http://localhost:3000", "http://13.209.227.43:3000", "http://dailyrecord.me", "*"],
+    origin: ["http://localhost:3000", "http://13.209.227.43:3000", "http://dailyrecord.me", "http://www.dailyrecord.me", "*"],
     credentials: true,
     sameSite: "none",
 }));
